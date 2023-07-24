df7f849ce4dd158ccda306e547fee80737ede9e4
Giogap <giogap@gmail.com>
2023-07-23 19:20:02 -0500

    upload db

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
 const mysql = require("mysql");
-require('dotenv').config();
+
 
 
 const app = express();
